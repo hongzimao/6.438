8301b96815e60be67d77f2ed7f2af08a3e6d3768
Hongzi Mao <hongzi@mit.edu>
2018-11-03 15:39:02 -0400

    ps5 up til 5.2d

---
+++ ps5/mean_var.py
@@ -0,0 +1,57 @@
+import numpy as np
+from scipy import misc
+
+
+def compute_mean_var(image, ground):
+    num_pixels = 0
+    mean = np.zeros(3)
+    sum_val = np.zeros(3)
+
+    assert image.shape[0] == ground.shape[0]
+    assert image.shape[1] == ground.shape[1]
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if ground[i, j] == 1:
+                sum_val += image[i, j]
+                num_pixels += 1
+
+    if num_pixels > 0:
+        mean = sum_val / num_pixels
+
+    var = np.zeros([3, 3])
+    sum_val = np.zeros([3, 3])
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if ground[i, j] == 1:
+                val = image[i, j].reshape(-1, 1)
+                sum_val += val.dot(val.T)
+
+    if num_pixels > 0:
+        var = sum_val / num_pixels
+
+    return mean, var
+
+
+def main():
+    image_file = './images/flower.bmp'
+    foreground_file = './images/foreground.bmp'
+    background_file = './images/background.bmp'
+
+    image = misc.imread(image_file, flatten= 0)
+    foreground = misc.imread(foreground_file, flatten= 0)
+    background = misc.imread(background_file, flatten= 0)
+
+    mean_foreground, var_foreground = \
+        compute_mean_var(image, foreground)
+
+    mean_background, var_background = \
+        compute_mean_var(image, background)
+
+    print('Mean foreground', mean_foreground, 'Var foreground', var_foreground)
+    print('Mean background', mean_background, 'Var background', var_background)
+
+
+if __name__ == '__main__':
+    main()
