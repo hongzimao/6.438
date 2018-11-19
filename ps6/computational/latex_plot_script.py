69bb576fa22dd16ad4e2a67cfd212116e8c1c8ce
Hongzi Mao <hongzi@mit.edu>
2018-11-19 12:31:58 -0500

    update figures

---
+++ ps6/computational/latex_plot_script.py
@@ -0,0 +1,43 @@
+tw = 0.18
+d = 0.01
+
+with open('latex_figures', 'w') as f:
+    for i in range(10):
+        f.write('\\begin{subfigure}[t]{' + str(tw) + '\\textwidth}\n')
+        f.write('\\centering\n')
+        f.write('\\includegraphics[width=\\textwidth]{./computational/results/gibbs_node_sampler_positive_iter_' + str(i * 100) + '.png}\n')
+        f.write('\\vspace{-0.6cm}\n')
+        f.write('\\caption{iteration=' + str(max(1, i * 100)) + '}\n')
+        f.write('\\end{subfigure}\\hspace{' + str(d) + '\\textwidth}\n')
+
+    f.write('\n\n\n\n')
+
+    for i in range(10):
+        f.write('\\begin{subfigure}[t]{' + str(tw) + '\\textwidth}\n')
+        f.write('\\centering\n')
+        f.write('\\includegraphics[width=\\textwidth]{./computational/results/gibbs_node_sampler_negative_iter_' + str(i * 100) + '.png}\n')
+        f.write('\\vspace{-0.6cm}\n')
+        f.write('\\caption{iteration=' + str(max(1, i * 100)) + '}\n')
+        f.write('\\end{subfigure}\\hspace{' + str(d) + '\\textwidth}\n')
+
+    f.write('\n\n\n\n')
+
+    for i in range(10):
+        f.write('\\begin{subfigure}[t]{' + str(tw) + '\\textwidth}\n')
+        f.write('\\centering\n')
+        f.write('\\includegraphics[width=\\textwidth]{./computational/results/gibbs_comb_sampler_positive_iter_' + str(i * 100) + '.png}\n')
+        f.write('\\vspace{-0.6cm}\n')
+        f.write('\\caption{iteration=' + str(max(1, i * 100)) + '}\n')
+        f.write('\\end{subfigure}\\hspace{' + str(d) + '\\textwidth}\n')
+
+    f.write('\n\n\n\n')
+
+    for i in range(10):
+        f.write('\\begin{subfigure}[t]{' + str(tw) + '\\textwidth}\n')
+        f.write('\\centering\n')
+        f.write('\\includegraphics[width=\\textwidth]{./computational/results/gibbs_comb_sampler_negative_iter_' + str(i * 100) + '.png}\n')
+        f.write('\\vspace{-0.6cm}\n')
+        f.write('\\caption{iteration=' + str(max(1, i * 100)) + '}\n')
+        f.write('\\end{subfigure}\\hspace{' + str(d) + '\\textwidth}\n')       
+
+
--- ps6/computational/plot.py
+++ ps6/computational/plot.py
@@ -9,6 +9,7 @@ def main():
     comb_positive = np.load('./results/gibbs_comb_sampler_positive_mixing.npy')
     comb_negative = np.load('./results/gibbs_comb_sampler_negative_mixing.npy')
     node_positive_long = np.load('./results/gibbs_node_sampler_positive_long_mixing.npy')
+    node_negative_long = np.load('./results/gibbs_node_sampler_negative_long_mixing.npy')
 
     font = {'family' : 'normal',
         'size'   : 14}
@@ -20,6 +21,7 @@ def main():
     plt.xlabel('Step')
     plt.ylabel('Average value \n of all nodes')
     plt.title('Gibbs node-by-node sampler')
+    plt.legend(['Initialized at 1', 'Initialized at -1'], frameon=False)
     plt.tight_layout()
     plt.savefig('gibbs_node_sampler_mixing.pdf')
     plt.close(fig)
@@ -30,15 +32,18 @@ def main():
     plt.xlabel('Step')
     plt.ylabel('Average value \n of all nodes')
     plt.title('Gibbs block sampler')
+    plt.legend(['Initialized at 1', 'Initialized at -1'], frameon=False)
     plt.tight_layout()
     plt.savefig('gibbs_block_sampler_mixing.pdf')
     plt.close(fig)
 
-    fig = plt.figure(figsize=(6, 4))
+    fig = plt.figure(figsize=(9, 4))
     plt.plot(node_positive_long)
+    plt.plot(node_negative_long)
     plt.xlabel('Step')
     plt.ylabel('Average value \n of all nodes')
     plt.title('Gibbs node-by-node sampler (longer)')
+    plt.legend(['Initialized at 1', 'Initialized at -1'], frameon=False, loc='center left', bbox_to_anchor=(1, 0.5))
     plt.tight_layout()
     plt.savefig('gibbs_node_sampler_mixing_longer.pdf')
     plt.close(fig)
